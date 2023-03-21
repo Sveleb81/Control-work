7923b7d8801258a31bef7b2cb481d04ed2190f6b
Sveleb81 <sveleb81@gmail.com>
2023-03-21 15:30:40 +0300

    program

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите число элементов массива: ");
+int arrLength = Convert.ToInt32(Console.ReadLine());
+string [] arr = new string[arrLength];
+string [] resultArr = new string[arrLength];
+
+Console.WriteLine("Заполните массив: ");
+for (int i = 0; i < arrLength; i++) {
+    Console.Write($"{i + 1}: ");
+    arr[i] = Console.ReadLine() ?? "";
+}
+
+int resultArrLength = 0;
+
+for (int i = 0; i < arrLength; i++) {
+    if (arr[i].Length < 4) {
+        resultArr[resultArrLength++] = arr[i];
+    };
+}
+
+Array.Resize(ref resultArr, resultArrLength);
+
+string strResultArr = "[";
+
+for (int i = 0; i < resultArrLength; i++) {
+    strResultArr += $"'{resultArr[i]}'";
+    if (i != resultArrLength - 1) strResultArr += ", ";
+}
+
+strResultArr += "]";
+
+Console.Write($"Получившийся массив: {strResultArr}");
+
